ff4b6310e852702a95bb44d7b359577ff982847f
softjeferson <jefersonalexandre224@gmail.com>
2023-04-27 17:26:29 -0300

    Update comandos.js

--- pagina-principal/estilos/comandos.js
+++ pagina-principal/estilos/comandos.js
@@ -18,5 +18,3 @@ if ( window.innerWidth >= 1024){
     var textJS = window.document.getElementById('textJS')
 textJS.innerHTML = `Eu sou um Densenvolvedor Front-End em estudo , disposto a novas Experiência no desenvolvimento de sites e aplicativos web. Trabalho com tecnologias modernas, como HTML5,CSS3,JAVASCRIPT,para criar interfaces de usuários atraentes e responsivas. sempre busco estar atualizado com as últimas tendências e tecnologias,a fim de oferecer aos meus clientes soluções inovadoras e eficazes.`
 }
-
-//Eu sou um Densenvolvedor Front-End em estudo , disposto a novas Experiência no desenvolvimento de sites e aplicativos web. Trabalho com tecnologias modernas, como HTML5,CSS3,JAVASCRIPT,para criar interfaces de usuários atraentes e responsivas. sempre busco estar atualizado com as últimas tendências e tecnologias,a fim de oferecer aos meus clientes soluções inovadoras e eficazes.
