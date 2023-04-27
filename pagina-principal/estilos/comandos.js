2cbbfd3f36f1699b6e04afa0789a838c6cdaf485
softjeferson <jefersonalexandre224@gmail.com>
2023-04-27 00:16:34 -0300

    correção bug (resolvido)

--- pagina-principal/estilos/comandos.js
+++ pagina-principal/estilos/comandos.js
@@ -7,25 +7,9 @@ function clicar(){
 }
 
 function mudouTamanho(){
-    if (window.innerWidth >= 768 ){
+    if (window.innerWidth >= 768){
         navegação.style.display = 'block'
-    } else if(window.innerWidth < 767)  {
-        navegação.style.display = 'none'
     } else {
-        navegação.style.display = 'block'
+        navegação.style.display = 'none'
     }
-
 }
-
-     let updatecount = 0
-
-     window,addEventListener('resize' , function(){
-        if (window.innerWidth <= 768){
-            if (updatecount < 1){
-                updatecount++;
-                location.reload();
-            }
-        } else {
-            updatecount = 0
-        }
-     });
