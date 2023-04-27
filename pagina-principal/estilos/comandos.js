5d427e171acd5acb22fa85a15062a7740166fd57
softjeferson <jefersonalexandre224@gmail.com>
2023-04-26 21:38:45 -0300

    correção bug

--- pagina-principal/estilos/comandos.js
+++ pagina-principal/estilos/comandos.js
@@ -1,13 +1,15 @@
 function clicar(){
-    if( navegação.style.display == 'block'){
-        navegação.style.display = 'none'
-    } else {
+    if( navegação.style.display == 'none'){
         navegação.style.display = 'block'
+    } else {
+        navegação.style.display = 'none'
     }
 }
 
 function mudouTamanho(){
-    if (window.innerWidth >= 768){
+    if (window.innerWidth >= 768 ){
+        navegação.style.display = 'block'
+    } else if(window.innerWidth < 767)  {
         navegação.style.display = 'none'
     } else {
         navegação.style.display = 'block'
