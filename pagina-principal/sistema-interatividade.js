bc4f228bb3adaae91485285b3a0134d04dc1b1fb
softjeferson <jefersonalexandre224@gmail.com>
2023-05-11 16:43:15 -0300

    1.2v (novo modelo de site )

--- pagina-principal/sistema-interatividade.js
+++ pagina-principal/sistema-interatividade.js
@@ -1,13 +1,10 @@
-//header
 function animar(){
     const btn = document.getElementById('btn-menu')
     btn.classList.toggle('ativar')
 
     menuDiv.classList.toggle('abrir')
-
 }
 
-const menuDiv = document.getElementById("menu-mobile")
+const menuDiv = document.getElementById('menu-mobile');
 
-menuDiv.addEventListener("click", animar )
-//header
+menuDiv.addEventListener("click", animar );
