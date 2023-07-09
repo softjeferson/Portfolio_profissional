0b572ad18f5dabbf383de9d1fc35c496811c69a7
JefersonSilvaa <jefersonalexandre224@gmail.com>
2023-07-09 13:55:13 -0300

    .

--- pagina-principal/faficon.io/sistema js/interatividade.js
+++
@@ -1,9 +0,0 @@
-//header btn-menu
-function clicar(){
-    const btn = window.document.getElementById("btn-menu")
-    btn.classList.toggle('ativar')
-
-    menuDiv.classList.toggle("abrir")
-}
-const menuDiv = document.getElementById("menu-mobile")
-menuDiv.addEventListener("click", clicar)
